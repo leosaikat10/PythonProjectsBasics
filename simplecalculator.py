1f5fb8d122fcfc0f37680cfc597f642563dc48d3
Saikat <saikatbiswas24dev@gmail.com>
2026-01-07 23:03:03 +0530

    Modified with functions

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,3 +1,18 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
+
 a = float(input("Enter the first number:"))
 b = float(input("Enter the second number:"))
 
@@ -10,15 +25,15 @@ print("4.Division")
 choice=input("Enter choice (1/2/3/4):")
 
 if choice == "1":
-  print("Result:",a+b)
+  print("Result:",add(a,b))
 elif choice == "2":
-  print("Result:",a-b)
+  print("Result:",subtract(a,b))
 elif choice == "3":
-  print("Result:",a*b)
+  print("Result:",multiply(a,b))
 elif choice == "4":
   if b == 0:
     print("Error: Division with zero is not allowed")
   else:
-    print("Result:",a/b)  
+    print("Result:",divide(a,b))  
 else:
   print("Invalid Choice")
