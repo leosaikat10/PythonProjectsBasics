ffc08a0a815f296b961b643cf16771a2bafd5941
Saikat <saikatbiswas24dev@gmail.com>
2026-01-07 22:47:05 +0530

    Minor issues solved

--- simplecalculator.py
+++ simplecalculator.py
@@ -7,19 +7,15 @@ print("3.Multiply")
 print("4.Division")
 
 
-choice=input("Enter choice (1/2/3/4)")
+choice=input("Enter choice (1/2/3/4):")
 
-if choice == 1:
+if choice == "1":
   print("Result:",a+b)
-
-elif choice == 2:
+elif choice == "2":
   print("Result:",a-b)
-
-elif choice == 3:
+elif choice == "3":
   print("Result:",a*b)
-
-elif choice == 4:
+elif choice == "4":
   print("Result:",a/b)
-
 else:
   print("Invalid Choice")
