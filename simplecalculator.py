15f789ab8c465416c78f2f51e8ffb38e47a25ec7
Saikat <saikatbiswas24dev@gmail.com>
2026-01-07 22:38:58 +0530

    multiplication & division added

--- simplecalculator.py
+++ simplecalculator.py
@@ -3,14 +3,23 @@ b = float(input("Enter the second number:"))
 
 print("1.Add")
 print("2.Substract")
+print("3.Multiply")
+print("4.Division")
 
-choice=input("Enter choice (1/2)")
+
+choice=input("Enter choice (1/2/3/4)")
 
 if choice == 1:
   print("Result:",a+b)
 
 elif choice == 2:
   print("Result:",a-b)
+
+elif choice == 3:
+  print("Result:",a*b)
+
+elif choice == 4:
+  print("Result:",a/b)
 
 else:
   print("Invalid Choice")
